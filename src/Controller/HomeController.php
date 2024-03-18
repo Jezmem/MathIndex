603394865e5779835938a160da85934bceb7b7c9
Yamosai <moncet.yannis@gmail.com>
2024-03-18 09:22:17 +0100

    push du setup du front

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class HomeController extends AbstractController 
+{
+    #[Route('/accueil', name:'accueil')]
+    public function accueil(): Response
+    {
+        return $this->render('homepage/index.html.twig');
+    }
+}
