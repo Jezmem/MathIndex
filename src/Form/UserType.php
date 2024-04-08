f1fd4bdcc638f2c4d1ee99060a16d867fbd00b4e
Yamosai <moncet.yannis@gmail.com>
2024-04-08 16:22:21 +0200

    correction des roles

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,15 +18,42 @@ class UserType extends AbstractType
             ->add('first_name')
             ->add('email')
             ->add('password')
-            ->add('roles')
         ;
+
+        // Add roles field only if the user doesn't have ROLE_ADMIN
+        if (!in_array('ROLE_ADMIN', $options['user_roles'])) {
+            $builder->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices'  => [
+                    'Étudiant' => 'ROLE_STUDENT',
+                    'Enseignant' => 'ROLE_TEACHER',
+                ],
+            ]);
+
+            $builder->get('roles')
+                ->addModelTransformer(new CallbackTransformer(
+                    function ($rolesArray) {
+                        // transform the array to a string
+                        return count($rolesArray) ? $rolesArray[0] : null;
+                    },
+                    function ($rolesString) {
+                        // transform the string back to an array
+                        return [$rolesString];
+                    }
+                ));
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'user_roles' => [], 
         ]);
     }
+
+
 }
     
