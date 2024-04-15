c1f9e2234033dd33faf887037d96301cf7e29a97
Jezmem <tomzarb98@gmail.com>
2024-04-15 11:07:55 +0200

    modif module recherche

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,15 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Classroom;
+use App\Entity\Thematic;
+use App\Entity\Exercise;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ExerciseRepository; // Importez ExerciseRepository
 
 class SearchController extends AbstractController
 {
-    #[Route('/search', name: 'search')]
-    public function accueil(): Response
+    private $entityManager;
+    private $exerciseRepository; // Déclarez la propriété $exerciseRepository
+
+    // Injecter EntityManagerInterface et ExerciseRepository dans le constructeur
+    public function __construct(EntityManagerInterface $entityManager, ExerciseRepository $exerciseRepository)
     {
-        return $this->render('search.html.twig');
+        $this->entityManager = $entityManager;
+        $this->exerciseRepository = $exerciseRepository; // Affectez le paramètre $exerciseRepository à la propriété $exerciseRepository
+    }
+    
+    #[Route('/search', name: 'search')]
+    public function accueil(Request $request): Response
+    {
+        $searchCriteria = [];
+        $searchCriteria['classroom'] = $request->request->get('classroom');
+        $searchCriteria['thematic'] = $request->request->get('thematic');
+        $searchCriteria['keyword'] = $request->request->get('keyword');
+        // Récupérer les critères de recherche depuis la requête
+        
+        $classrooms = $this->entityManager->getRepository(Classroom::class)->findAll();
+        $thematics = $this->entityManager->getRepository(Thematic::class)->findAll();
+        // Utilisez $this->exerciseRepository pour rechercher les exercices
+        $exercises = $this->exerciseRepository->findBySearchCriteria($searchCriteria);
+
+        return $this->render('search.html.twig', [
+            'classrooms' => $classrooms,
+            'thematics' => $thematics,
+            'exercises' => $exercises,
+            'searchCriteria' => $searchCriteria,
+        ]);
     }
 }
--- src/Repository/ExerciseRepository.php
+++ src/Repository/ExerciseRepository.php
@@ -21,20 +21,20 @@ class ExerciseRepository extends ServiceEntityRepository
         parent::__construct($registry, Exercise::class);
     }
 
-//    /**
-//     * @return Exercise[] Returns an array of Exercise objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   /**
+    * @return Exercise[] Returns an array of Exercise objects
+    */
+   public function findByExampleField($value): array
+   {
+       return $this->createQueryBuilder('e')
+           ->andWhere('e.exampleField = :val')
+           ->setParameter('val', $value)
+           ->orderBy('e.id', 'ASC')
+           ->setMaxResults(10)
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 
 //    public function findOneBySomeField($value): ?Exercise
 //    {
@@ -45,4 +45,30 @@ class ExerciseRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
+public function findBySearchCriteria($criteria)
+{
+    $queryBuilder = $this->createQueryBuilder('e');
+
+    // Ajout des critères de recherche si disponibles
+    if (!empty($criteria['difficulty'])) {
+        $queryBuilder->andWhere('e.difficulty = :difficulty')
+            ->setParameter('difficulty', $criteria['difficulty']);
+    }
+
+    if (!empty($criteria['thematic'])) {
+        $queryBuilder->andWhere('e.thematic = :thematic')
+            ->setParameter('thematic', $criteria['thematic']);
+    }
+
+    if (!empty($criteria['keyword'])) {
+        $queryBuilder->andWhere('e.name LIKE :keyword')
+            ->setParameter('keyword', '%' . $criteria['keyword'] . '%');
+    }
+
+    // Exemple : tri par nom de manière ascendante
+    $queryBuilder->orderBy('e.name', 'ASC');
+
+    // Exécutez la requête et retournez le résultat
+    return $queryBuilder->getQuery()->getResult();
+}
 }
